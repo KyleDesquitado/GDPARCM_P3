4887fe2ea95a72a723a5c856aba956e6a08fe057
Set Olivarez <setiel_b_olivarez@dlsu.edu.ph>
2023-04-12 21:43:17 +0800

    Added Semaphore class

---
+++ SampleDirectXProject/IETSemaphore.cpp
@@ -0,0 +1,35 @@
+#include "IETSemaphore.h"
+#include "IETSemaphore.h"
+
+IETSemaphore::IETSemaphore(int available)
+{
+	this->semaphore = new Semaphore(available);
+}
+
+IETSemaphore::~IETSemaphore()
+{
+	delete this->semaphore;
+}
+
+void IETSemaphore::acquire() const
+{
+	this->semaphore->acquire();
+}
+
+void IETSemaphore::acquire(int permits) const
+{
+	for (int i = 0; i < permits; i++)
+	{
+		this->semaphore->acquire();
+	}
+}
+
+void IETSemaphore::release(int permits) const
+{
+	this->semaphore->release(permits);
+}
+
+void IETSemaphore::release() const
+{
+	this->semaphore->release();
+}
